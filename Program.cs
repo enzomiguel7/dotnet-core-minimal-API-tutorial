ba207fa9ce19eb7ca6453d382e1c541f8ff24f17
enzo <enzooliveirasilva90@gmail.com>
2026-02-12 18:59:56 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+//configuração do banco de dados em memória;
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//constroi a aplicação com as configurações acima 
 var app = builder.Build();
 
-app.MapGet("/", () => "I Like trip hop and downtempo!");
+app.MapGet("/todoitems", async (TodoDb db) => 
+    await db.Todos.ToListAsync());
 
-app.Run();
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems{id}", async (int id, TodoDb db) => 
+    await db.Todos.FindAsync(id)
+         is Todo todo
+             ? Results.Ok(todo)
+             : Results.NotFound());
---
+++ Todo.cs
@@ -0,0 +1,7 @@
+// Essa classe é o modelo de como serão os dados do programa
+public class Todo
+{
+    public int Id{get; set;}
+    public string? Name {get; set;}
+    public bool IsComplete {get; set;}
+}
