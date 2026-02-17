af2bace122492cee542b8dce14d9db981361272a
enzo <enzooliveirasilva90@gmail.com>
2026-02-17 14:14:05 -0300

    adicionado swagger

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-//configuração do banco de dados em memória;
+
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-//constroi a aplicação com as configurações acima 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+
+}
 
 app.MapGet("/todoitems", async (TodoDb db) => 
     await db.Todos.ToListAsync());
@@ -13,7 +33,7 @@ app.MapGet("/todoitems", async (TodoDb db) =>
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems{id}", async (int id, TodoDb db) => 
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => 
     await db.Todos.FindAsync(id)
          is Todo todo
              ? Results.Ok(todo)
